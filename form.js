1d1d65999858c516c043a1321a2481405f30a9c9
Nemolite <g25092011@mail.ru>
2026-02-04 16:25:07 +0300

    Create form.js
    sdfsad

---
+++ form.js
@@ -0,0 +1,14 @@
+// function sendForm(e) {
+// 	alert('Все работает')
+// }
+
+// const sendButton = document.main_form.btn
+// sendButton.addEventListener('click', sendForm)
+
+const headerElement = document.getElementById('head_txt')
+console.log('Text: ', headerElement.innerText)
+
+const d = document.querySelector('#head_txt')
+console.log(d.innerText)
+
+d.innerText = 'ttttttttt'
