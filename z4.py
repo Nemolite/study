75339603f498ddfefcb21c59ec9eabcd8b7114cb
Nemolite <g25092011@mail.ru>
2026-05-07 10:56:37 +0300

    Create z4.py
    Задача 4

---
+++ z4.py
@@ -0,0 +1,24 @@
+def f0(n,m):
+    # Преобразовать в массив и выполнить поиск
+    # return True
+    pass
+
+def f2(n,k):
+    pass
+
+n = int(input("Введите разрядность числа = "))
+ch = []
+for t in range(n):
+    ch.append('9')
+result = "".join(ch)
+v = int(result)+1
+
+for i in range(v):
+    print(i)
+
+    # if f0(n,m)&f2(n,k):
+        # накопить числа в массив
+        # предварительно переведем число в девятеричный код
+        # arr
+
+# print(max(arr))
